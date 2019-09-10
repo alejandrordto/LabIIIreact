53ce8a144267818c9a854c4be9e0799cba12aee7
alejandrordto <masterale2011@gmail.com>
2019-09-10 12:42:08 -0500

    button

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo, { ReactComponent } from './logo.svg';
+import Button from '@material-ui/core/Button';
 import { TodoList } from './TodoList';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import Face from '@material-ui/icons/Face';
+import Menu from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 class TodoApp extends Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '', priority: '', dueDate: '' };
+    this.state = { items: [], text: '', priority: '', dueDate: '', side: false };
     this.handletext = this.handletext.bind(this);
     this.handlePriorityChange = this.handlePriorityChange.bind(this);
     this.handleDateChange = this.handleDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleside = this.handleside.bind(this);
   }
   handletext(e) {
     this.setState({ text: e.target.value });
+  }
+  handleside(e) {
+    if (this.state.side) {
+      this.setState({ side: false });
+    } else {
+      this.setState({ side: true });
+    }
+    console.log(this.state.side)
+
   }
   handlePriorityChange(e) {
     this.setState({ priority: e.target.value });
@@ -38,20 +57,43 @@ class TodoApp extends Component {
       dueDate: ''
     }))
 
-  
+
   }
   render() {
-    const ListItems = [{ text: "Learn React", priority: 5, dueDate: new Date() },
-    { text: "Learn about APIs", priority: 4, dueDate: new Date(2018, 8, 30) },
-    { text: "write TODO App", priority: 3, dueDate: new Date(2018, 9, 30) }];
 
+    const toggleDrawer = (side, open) => event => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+    }
     return (
       <div className="TodoApp">
+        <div>
+          <IconButton className="btn" aria-label="Menu" onClick={this.handleside}>
+            <Menu></Menu>
+          </IconButton>
+        </div>
+        <Drawer open={this.state.side}>
+          <div
+            role="presentation"
+          ></div>
+          <List>
+            <ListItem key='User'>
+              <ListItemIcon><Face></Face></ListItemIcon>
+              <ListItemText primary={localStorage.getItem('email')} />
+            </ListItem>
 
+          </List>
+          <Divider />
+          <Button variant="contained" color="secondary" onClick={this.handleside}>
+         LOG OUT
+      </Button>
+        </Drawer>
+        
         <div>
           <h2>TodoList</h2>
-          
-          <h2>hola {localStorage.getItem('Usuario')}</h2>
+
+          <h2>hola {localStorage.getItem('email')}</h2>
           <form onSubmit={this.handleSubmit}>
             <input
               id="texto"
@@ -83,8 +125,6 @@ class TodoApp extends Component {
           </form>
         </div>
         <TodoList items={this.state.items} />
-        <img src={logo} className="App-logo" alt="logo" />
-
       </div>
 
     );
